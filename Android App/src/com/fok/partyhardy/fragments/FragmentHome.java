e441374e32c2f9c5637a44fa8f6a376703efbe06
sirolf2009 <masterflappie@gmail.com>
2014-05-16 10:40:50 +0200

    Cleaner Emtpy App Project

---
+++ Android App/src/com/fok/partyhardy/fragments/FragmentHome.java
@@ -0,0 +1,24 @@
+package com.fok.partyhardy.fragments;
+
+import com.fok.partyhardy.R;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class FragmentHome extends Fragment {
+
+	public FragmentHome() {
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_home, container, false);
+	}
+
+}
